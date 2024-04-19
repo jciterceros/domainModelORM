cd5552ea15703708777f51ba0724d0a6100bde4a
jciterceros <jciterceros@yahoo.com>
2024-04-19 08:09:00 -0300

    Initial Commit

---
+++ src/main/java/com/jciterceros/domainModelORM/DomainModelOrmApplication.java
@@ -0,0 +1,13 @@
+package com.jciterceros.domainModelORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainModelOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainModelOrmApplication.class, args);
+	}
+
+}
